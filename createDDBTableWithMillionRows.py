737129ccc86538f9f3f45e3013c4481ae73696f1
bormihir <bormihir@amazon.com>
2020-04-28 22:00:54 +0000

    Edited createDDBTableWithMillionRows.py

--- createDDBTableWithMillionRows.py
+++ createDDBTableWithMillionRows.py
@@ -4,3 +4,4 @@ client=boto3.client('dynamodb')
 
 for i in range(100,1000000):
     response = client.put_item(TableName='product',Item={"product_id": {"N": str(i) },"version": {"N": "23"},"pro_ver": {"N": str(i) },"Name": {"S": "Testname this is for test" },"order_id": {"N": str(i+10) }})
+    
